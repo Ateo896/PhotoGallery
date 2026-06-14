33ae58cceb4c94cc39eb69fd4d673d86e7dee27a
Ateo <tereshkov896@gmail.com>
2026-06-15 04:25:20 +0700

    Add RetrofitInstance.kt

---
+++ app/src/main/java/com/example/photogallery/data/RetrofitInstance.kt
@@ -0,0 +1,31 @@
+package com.sample.photogallery.data
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object RetrofitInstance {
+    private const val BASE_URL = "https://api.flickr.com/"
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(client)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .build()
+
+    val api: FlickrApi = retrofit.create(FlickrApi::class.java)
+}
